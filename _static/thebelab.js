51ab771a0fafd9af1253998a44b7bd542f744050
mmcky <mmcky@users.noreply.github.com>
2020-07-06 00:20:52 +0000

    Deploying to gh-pages from  @ 6f488684cea4aa081952b0a6c202e8fdcc1bb666 🚀

--- _static/thebelab.js
+++ _static/thebelab.js
@@ -3,73 +3,94 @@
  */
 
 var initThebelab = () => {
-    console.log("Adding thebelab to code cells...");
-
-    thebe_config = $('script[type="text/x-thebe-config"]')[0]
-
     // If Thebelab hasn't loaded, wait a bit and try again. This
     // happens because we load ClipboardJS asynchronously.
     if (window.thebelab === undefined) {
-        setTimeout(addThebelabToCodeCells, 250)
-    return
+        console.log("thebelab not loaded, retrying...");
+        setTimeout(initThebelab, 500)
+        return
     }
 
+    console.log("Adding thebelab to code cells...");
+
+    // Load thebe config in case we want to update it as some point
+    thebe_config = $('script[type="text/x-thebe-config"]')[0]
+
+
     // If we already detect a Thebelab cell, don't re-run
-    if (document.querySelectorAll('div.thebelab-cell').length > 0) {
+    if (document.querySelectorAll('div.thebe-cell').length > 0) {
         return;
     }
 
-    // Find all code cells, replace with Thebelab interactive code cells
-    const codeCells = document.querySelectorAll('.cell_input pre')
-    codeCells.forEach((codeCell, index) => {
-        const id = codeCellId(index)
+    // Update thebelab buttons with loading message
+    $(".thebe-launch-button").each((ii, button) => {
+        button.innerHTML = `
+        <div class="spinner">
+            <div class="rect1"></div>
+            <div class="rect2"></div>
+            <div class="rect3"></div>
+            <div class="rect4"></div>
+        </div>
+        <span class="loading-text"></span>`;
+    })
 
-        // Clean up the language to make it work w/ CodeMirror and add it to the cell
-        dataLanguage = "{{ kernelName }}"
-        dataLanguage = detectLanguage(dataLanguage);
-        codeCell.setAttribute('data-language', dataLanguage)
-        codeCell.setAttribute('data-executable', 'true')
+    // Set thebelab event hooks
+    var thebelabStatus;
+    thebelab.on("status", function (evt, data) {
+        console.log("Status changed:", data.status, data.message);
 
-        // If the code cell is hidden, show it
-        var inputCheckbox = document.querySelector(`input#hidebtn${codeCell.id}`);
-        if (inputCheckbox !== null) {
-            setCodeCellVisibility(inputCheckbox, 'visible');
+        $(".thebe-launch-button ")
+        .removeClass("thebe-status-" + thebelabStatus)
+        .addClass("thebe-status-" + data.status)
+        .find(".loading-text").html("<span class='launch_msg'>Launching from mybinder.org: </span><span class='status'>" + data.status + "</span>");
+
+        // Now update our thebelab status
+        thebelabStatus = data.status;
+
+        // Find any cells with an initialization tag and ask ThebeLab to run them when ready
+        if (data.status === "ready") {
+            var thebeInitCells = document.querySelectorAll('.thebe-init, .tag_thebe-init');
+            thebeInitCells.forEach((cell) => {
+                console.log("Initializing ThebeLab with cell: " + cell.id);
+                cell.querySelector('.thebelab-run-button').click();
+            });
         }
     });
 
-    // Remove the event listener from the page so keyboard press doesn't
-    // Change page
-    // TODO: old code to do this commented below
-    // document.removeEventListener('keydown', initPageNav)
-    // keyboardListener = false;
+
+    // Find all code cells, replace with Thebelab interactive code cells
+    const codeCells = document.querySelectorAll(thebe_selector)
+    codeCells.forEach((codeCell, index) => {
+        const codeCellId = index => `codecell${index}`;
+        codeCell.id = codeCellId(index);
+        codeCellText = codeCell.querySelector(thebe_selector_input);
+        codeCellOutput = codeCell.querySelector(thebe_selector_output);
+
+        // Clean up the language to make it work w/ CodeMirror and add it to the cell
+        dataLanguage = detectLanguage(kernelName);
+
+        if (codeCellText) {
+            codeCellText.setAttribute('data-language', dataLanguage);
+            codeCellText.setAttribute('data-executable', 'true');
+
+            // If we had an output, insert it just after the `pre` cell
+            if (codeCellOutput) {
+                $(codeCellOutput).attr("data-output", "");
+                $(codeCellOutput).insertAfter(codeCellText);
+            }
+        }
+    });
 
     // Init thebelab
     thebelab.bootstrap();
-
-    // Remove copy buttons since they won't work anymore
-    const copyAndThebeButtons = document.querySelectorAll('.copybtn')
-    copyAndThebeButtons.forEach((button, index) => {
-        button.remove();
-    });
-
-    // Remove outputs since they'll be stale
-    const outputs = document.querySelectorAll('.cell_output')
-    outputs.forEach((output, index) => {
-        output.remove();
-    });
-
-    // Find any cells with an initialization tag and ask ThebeLab to run them when ready
-    var thebeInitCells = document.querySelectorAll('div.tag_thebelab-init');
-    thebeInitCells.forEach((cell) => {
-        console.log("Initializing ThebeLab with cell: " + cell.id);
-        cell.querySelector('.thebelab-run-button').click();
-    });
 }
 
 // Helper function to munge the language name
 var detectLanguage = (language) => {
     if (language.indexOf('python') > -1) {
         language = "python";
+    } else if (language === 'ir') {
+        language = "r"
     }
     return language;
 }
